227a47da5299188b04baea16cc7b9c4954a9f9d7
Jennifer Joseph <josephjz@bc.edu>
2020-09-06 11:01:21 -0400

    Finished code and user interface.

--- Founders Project/ViewController.swift
+++ Founders Project/ViewController.swift
@@ -14,7 +14,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var founder1Label: UILabel!
+    
+    @IBOutlet weak var founder2Label: UILabel!
+    
+    @IBAction func googleFounderButtonPressed(_ sender: UIButton) {
+        print("google founders shown.")
+        founder1Label.text = "Larry Page"
+        founder2Label.text = "Sergey Brin"
+    }
+    
+    @IBAction func rtrFounderButtonPressed(_ sender: UIButton) {
+        print("rtr founders shown.")
+        founder1Label.text = "Jennifer Hyman"
+        founder2Label.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        print("cleared.")
+        founder1Label.text = ""
+        founder2Label.text = ""
+    }
+    
+    
 }
 
